b6958d85da21ed5159e37843fc364775f1fddde0
Muhammad Mahad <146198984+MMahadAli@users.noreply.github.com>
2025-07-07 21:19:37 +0500

    Updated

--- scripts.js
+++ scripts.js
@@ -1,105 +1,50 @@
 const fileInput = document.getElementById('fileInput');
-const compressButton = document.getElementById('compressButton');
-const decompressButton = document.getElementById('decompressButton');
-const downloadButton = document.getElementById('downloadButton');
-const downloadDecompressedButton = document.getElementById('downloadDecompressedButton');
-const outputDiv = document.getElementById('output');
+        const compressButton = document.getElementById('compressButton');
+        const decompressButton = document.getElementById('decompressButton');
+        const outputDiv = document.getElementById('output');
 
-class Node {
-    constructor(char, freq) {
-        this.char = char;
-        this.freq = freq;
-        this.left = null;
-        this.right = null;
-    }
-}
+        compressButton.onclick = async () => {
+            const file = fileInput.files[0];
+            if (!file) {
+                outputDiv.innerText = 'Please select a file to compress.';
+                return;
+            }
 
-function buildHuffmanTree(freqMap) {
-    const priorityQueue = [];
+            const zip = new JSZip();
+            zip.file(file.name, file);
 
-    // Create nodes and add to priority queue
-    for (const char in freqMap) {
-        const node = new Node(char, freqMap[char]);
-        priorityQueue.push(node);
-    }
+            const content = await zip.generateAsync({ type: "blob" });
 
-    // Sort priority queue by frequency
-    priorityQueue.sort((a, b) => a.freq - b.freq);
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(content);
+            link.download = "compressed_file.zip";
+            link.click();
 
-    // Build Huffman tree
-    while (priorityQueue.length > 1) {
-        const node1 = priorityQueue.shift();
-        const node2 = priorityQueue.shift();
-
-        const newNode = new Node(null, node1.freq + node2.freq);
-        newNode.left = node1;
-        newNode.right = node2;
-
-        priorityQueue.push(newNode);
-        priorityQueue.sort((a, b) => a.freq - b.freq);
-    }
-
-    return priorityQueue[0];
-}
-
-function generateHuffmanCodes(node, currentCode, huffmanCodes) {
-    if (node === null) return;
-
-    if (node.char !== null) {
-        huffmanCodes[node.char] = currentCode;
-    }
-
-    generateHuffmanCodes(node.left, currentCode + '0', huffmanCodes);
-    generateHuffmanCodes(node.right, currentCode + '1', huffmanCodes);
-}
-
-function compressFile(file) {
-    const reader = new FileReader();
-    reader.onload = () => {
-        const fileContent = reader.result;
-        const freqMap = {};
-
-        for (let i = 0; i < fileContent.length; i++) {
-            const char = fileContent[i];
-            freqMap[char] = (freqMap[char] || 0) + 1;
-        }
-
-        const huffmanTree = buildHuffmanTree(freqMap);
-        const huffmanCodes = {};
-        generateHuffmanCodes(huffmanTree, '', huffmanCodes);
-
-        let compressedContent = '';
-        for (let i = 0; i < fileContent.length; i++) {
-            compressedContent += huffmanCodes[fileContent[i]];
-        }
-
-        const binaryString = [];
-        for (let i = 0; i < compressedContent.length; i += 8) {
-            binaryString.push(parseInt(compressedContent.substring(i, i + 8), 2));
-        }
-
-        const compressedFile = new Blob([new Uint8Array(binaryString)], { type: 'application/octet-stream' });
-        const url = URL.createObjectURL(compressedFile);
-
-        downloadButton.disabled = false;
-        downloadButton.onclick = () => {
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'compressed_file.huf';
-            a.click();
+            outputDiv.innerText = 'File compressed into ZIP successfully!';
         };
 
-        outputDiv.innerText = 'File compressed successfully!';
-    };
-    reader.readAsBinaryString(file);
-}
+        decompressButton.onclick = async () => {
+            const file = fileInput.files[0];
+            if (!file) {
+                outputDiv.innerText = 'Please select a ZIP file to decompress.';
+                return;
+            }
 
-fileInput.onchange = () => {
-    compressButton.disabled = false;
-    decompressButton.disabled = false;
-};
+            const zip = new JSZip();
+            const content = await file.arrayBuffer();
+            const unzipped = await zip.loadAsync(content);
 
-compressButton.onclick = () => {
-    const file = fileInput.files[0];
-    compressFile(file);
-};
+            outputDiv.innerHTML = '';
+
+            Object.keys(unzipped.files).forEach(async (fileName) => {
+                const fileData = await unzipped.files[fileName].async('blob');
+
+                const link = document.createElement('a');
+                link.href = URL.createObjectURL(fileData);
+                link.download = fileName;
+                link.innerText = `Download ${fileName}`;
+                link.className = "btn btn-success mt-2 me-2";
+
+                outputDiv.appendChild(link);
+            });
+        }
